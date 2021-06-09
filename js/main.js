26f52ff32990645a2a2f126e7c5605b0971e397a
streetsummit <streetsummit@mail.ru>
2021-06-10 00:24:56 +0300

    Заменяет функцию получения рандомного числа

--- js/main.js
+++ js/main.js
@@ -1,20 +1,13 @@
-const getRandomNumber = function(min, max, decimalPlaces = 0) {
-  if (min < 0 || max < 0 || decimalPlaces < 0) {
-    throw new Error('Передаваемые значения не могут быть отрицательными');
-  }
-
-  if (min >= max) {
-    throw new Error('Минимальное значение не может быть больше максимального или равно ему');
-  }
-
-  max = Math.floor(max * 10 ** decimalPlaces) / 10 ** decimalPlaces;
-  min = Math.ceil(min * 10 ** decimalPlaces) / 10 ** decimalPlaces;
-
-  if ((max - min).toFixed(decimalPlaces) < Math.pow(10, -decimalPlaces) && (max !== min)) {
-    throw new Error('Нет чисел, удовлетворяющих условиям');
-  }
-  return (min + Math.random() * (max - min)).toFixed(decimalPlaces);
+const getRandomPositiveInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
 
-getRandomNumber(10.5, 11.5);
-getRandomNumber(5, 7.5);
+const getRandomPositiveFloat = (min, max, digits = 1) => {
+  const lower = Math.min(Math.abs(min), Math.abs(max));
+  const upper = Math.max(Math.abs(min), Math.abs(max));
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(digits);
+};
