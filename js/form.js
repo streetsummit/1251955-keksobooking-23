dfe652c18385bd374c87f8551549b65708b3bbc3
streetsummit <streetsummit@mail.ru>
2021-06-23 02:45:46 +0300

    Добавляет валидацию количества комнат и гостей

--- js/form.js
+++ js/form.js
@@ -4,6 +4,11 @@ const adFormElement = document.querySelector('.ad-form');
 const priceInput = adFormElement.querySelector('#price');
 const typeSelect = adFormElement.querySelector('#type');
 
+const roomNumberSelect = adFormElement.querySelector('#room_number');
+
+const guestNumberSelect = adFormElement.querySelector('#capacity');
+const guestNumberOptions = guestNumberSelect.querySelectorAll('option');
+
 const setMinPrice = () => {
   const minPriceValue = typesDictionary[typeSelect.value].price;
   priceInput.setAttribute('placeholder', minPriceValue);
@@ -15,6 +20,23 @@ const setFormValidity = () => {
     setMinPrice();
   });
 
+  roomNumberSelect.addEventListener('change', (evt) => {
+    const roomNumber = +evt.target.value;
+
+    guestNumberOptions.forEach((element, index) => {
+      if (roomNumber === 100 && +element.value === 0) {
+        element.disabled = false;
+        guestNumberSelect.selectedIndex = index;
+      } else if (roomNumber === 100 && +element.value !== 0 || +element.value === 0 || +element.value > roomNumber) {
+        element.disabled = true;
+      } else {
+        element.disabled = false;
+        guestNumberSelect.selectedIndex = index;
+      }
+    });
+  });
 };
 
 export {setFormValidity};
+
+
