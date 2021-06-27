fdeb0f5cb25f453a353583730d6f451794ac7ebd
streetsummit <streetsummit@mail.ru>
2021-06-27 15:35:10 +0300

    Удаляет временный код отрисовки объявления

--- js/main.js
+++ js/main.js
@@ -1,19 +1,9 @@
 import './mocks/data.js';
-import {createOfferList} from './mocks/data.js';
-import {createPopupMarkup} from './card.js';
 import {disableAdForm, activateAdForm, setFormValidity} from './form.js';
 import {disableFilterForm, activateFilterForm} from './filter.js';
 
-const OFFERS_COUNT = 10;
-
-const offers = createOfferList(OFFERS_COUNT);
-
-const map = document.querySelector('#map-canvas');
-
 disableAdForm();
 disableFilterForm();
-
-map.append(createPopupMarkup(offers[0]));
 
 activateAdForm();
 activateFilterForm();
