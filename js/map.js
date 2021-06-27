6f65b913812237d5155232073992dec156dde5f1
streetsummit <streetsummit@mail.ru>
2021-06-27 18:12:23 +0300

    Добавляет метки объявлений

--- js/map.js
+++ js/map.js
@@ -1,12 +1,14 @@
 import { activateAdForm, setAddress } from './form.js';
 import { activateFilterForm } from './filter.js';
+import { createOfferList } from './mocks/data.js';
+import { createPopupMarkup } from './card.js';
 
 const INITIAL_POINT = {
   lat: 35.68950,
   lng: 139.69171,
 };
 const INITIAL_MAP_ZOOM = 10;
-
+const OFFERS_COUNT = 10;
 
 const map = L.map('map-canvas');
 
@@ -26,6 +28,34 @@ const mainPin = L.marker(
 mainPin.on('drag', (evt) => {
   setAddress(evt.target.getLatLng());
 });
+
+const offers = createOfferList(OFFERS_COUNT);
+
+const pinIcon = L.icon({
+  iconUrl: '../img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 0]},
+);
+
+const createPin = (data) => {
+  const {location, ...rest} = data;
+  const pin = L.marker(
+    location,
+    {
+      icon: pinIcon,
+    },
+  );
+  pin
+    .addTo(map)
+    .bindPopup(
+      createPopupMarkup(rest),
+    );
+};
+
+offers.forEach((offer) => {
+  createPin(offer);
+});
+
 
 const initMap = () => {
   map
